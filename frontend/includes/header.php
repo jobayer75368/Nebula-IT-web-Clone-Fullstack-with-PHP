4d2e4646756e92360783f72ec645b6f2115c6c3f
Jobayer75368 <jobayer30325@gmail.com>
2026-05-23 19:40:58 +0600

    active_page class addded for navigation

--- frontend/includes/header.php
+++ frontend/includes/header.php
@@ -1,3 +1,8 @@
+<?php
+$currentPage = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+?>
+
 <header class="h-20 bg-white flex items-center shadow-md shadow-black/15 sticky top-0 z-40">
     <div class="container mx-auto px-5 lg:px-20 flex justify-between items-center">
         <div class="logo">
@@ -5,15 +10,15 @@
         </div>
         <nav class="font-semibold">
             <ul class="flex gap-8">
-                <li><a class="active_page" href="/">Home</a></li>
-                <li><a class="about_p" href="/about">About</a><i class="fa-solid fa-chevron-down text-xs"></i>
+                <li><a class=" <?php if ($currentPage == '/') echo 'active_page'; ?>" href="/">Home</a></li>
+                <li><a class="about_p <?php if ($currentPage == '/about') echo 'active_page'; ?>" href="/about">About</a><i class="fa-solid fa-chevron-down text-xs"></i>
                 </li>
-                <li><a class="service_p" href="/services">Services</a><i
+                <li><a class="service_p <?php if ($currentPage == '/services') echo 'active_page'; ?>" href="/services">Services</a><i
                         class="fa-solid fa-chevron-down text-xs"></i></li>
-                <li><a href="/portfolio">Portfolio</a></li>
-                <li><a href="/clients">Our Clients</a></li>
-                <li><a href="/blogs">Blog</a></li>
-                <li><a href="/contact">Contact</a></li>
+                <li><a class="<?php if ($currentPage == '/portfolio') echo 'active_page'; ?>" href="/portfolio">Portfolio</a></li>
+                <li><a class="<?php if ($currentPage == '/clients') echo 'active_page'; ?>" href="/clients">Our Clients</a></li>
+                <li><a class="<?php if ($currentPage == '/blogs') echo 'active_page'; ?>" href="/blogs">Blog</a></li>
+                <li><a class="<?php if ($currentPage == '/contact') echo 'active_page'; ?>" href="/contact">Contact</a></li>
             </ul>
         </nav>
         <div>
