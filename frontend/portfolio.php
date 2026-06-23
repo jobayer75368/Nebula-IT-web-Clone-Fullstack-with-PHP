66da49ee083a0eaec283c803755a7cc59186fe7c
Jobayer75368 <jobayer30325@gmail.com>
2026-06-23 15:47:40 +0600

    more portfolio added

--- frontend/portfolio.php
+++ frontend/portfolio.php
@@ -30,227 +30,24 @@ $allPortfolios = $allPtflStmt->fetchAll(PDO::FETCH_ASSOC);
     <?php require_once __DIR__ . '/includes/page_hero.php' ?>
     <section class="portfolio_section py-8 my-20">
         <div class="container mx-auto px-5 lg:px-20">
-            <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">]
+
                 <?php if (!empty($allPortfolios)): ?>
                     <?php foreach ($allPortfolios as $singlePortfolio): ?>
+
                         <div class="preview_card p-4">
                             <div class="grid_site">
                                 <iframe src="<?= !empty($singlePortfolio['website_link']) ? $singlePortfolio['website_link'] : ''; ?>"></iframe>
                             </div>
                             <div class="grid_text flex flex-col justify-center items-center">
-                                <h4 class="font-semibold"><?= !empty($singlePortfolio['website_name']) ? $singlePortfolio['website_name'] : ''; ?> <?= !empty($singlePortfolio['type']) ? $singlePortfolio['type'] : ''; ?></h4>
+                                <h4 class="font-semibold"><?= !empty($singlePortfolio['website_name']) ? $singlePortfolio['website_name'] : ''; ?> (<?= !empty($singlePortfolio['type']) ? $singlePortfolio['type'] : ''; ?>)</h4>
                                 <a class="secondary_link" href="<?= !empty($singlePortfolio['website_link']) ? $singlePortfolio['website_link'] : ''; ?>" target="_blank">Visit Website</a>
                             </div>
                         </div>
+
                     <?php endforeach; ?>
                 <?php endif; ?>
-                <!-- <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://angelrosebeautycarebd.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Beauty & Personal Care (eCommerce)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://intostudy.net/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Education Consultancy (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://taskininterior.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Interior Design (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://meklasca.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">CA (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://arcgroup-bd.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Business/Corporate (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://www.sunrise-bd.net/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Education Consultancy (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://studyitalybd.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Education Consultancy (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://www.globalrisebd.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Education Consultancy (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://triptravello.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Travel (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div> -->
-                <!-- <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://beautyeverthailand.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Beauty & Cosmetics (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://sekibeauty.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Beauty & Cosmetics (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://khalidbazar.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Departmental Store (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://www.ihdbd.org/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Healthcare (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://art.nebulaitbd.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Exhibition (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://srgb.org/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Corporate (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://innovatorit.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Graphics (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://coolingspaces.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Corporate (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://rahmananis.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">CA (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://falakcloset.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Fashion (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://daisybooksbd.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Book Store (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://nebulaclippingpath.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Graphics (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://mazamanca.com/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">CA (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div>
-                <div class="preview_card p-4">
-                    <div class="grid_site">
-                        <iframe src="https://mastereducationbd.net/"></iframe>
-                    </div>
-                    <div class="grid_text flex flex-col justify-center items-center">
-                        <h4 class="font-semibold">Education Consultancy (Portfolio)</h4>
-                        <a class="secondary_link" href="">Visit Website</a>
-                    </div>
-                </div> -->
+
             </div>
         </div>
     </section>
