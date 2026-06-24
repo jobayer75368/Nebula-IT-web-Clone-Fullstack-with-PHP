3e636a0ea7042829822bfc2988ba7b6245e54b2d
Jobayer75368 <jobayer30325@gmail.com>
2026-06-24 15:54:22 +0600

    frontend sidebar active_page added

--- frontend/includes/sidebar.php
+++ frontend/includes/sidebar.php
@@ -1,3 +1,13 @@
+<?php
+
+require_once __DIR__ . '/../../backend/includes/db_connection.php';
+
+$currentPage = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+
+
+?>
+
 <div class="sideBar bg-white z-50 fixed h-screen w-screen font-semibold">
     <div class="flex justify-end w-100">
         <button
@@ -6,16 +16,16 @@
         </button>
     </div>
     <ul class="flex flex-col gap-8 pl-5 ">
-        <li class="text-[var(--primary-color)] transition-all duration-300"><a href="/">Home</a></li>
-        <li class="hover:text-[var(--primary-color)] transition-all duration-300"><a href="/about">About</a><i
+        <li class="hover:text-[var(--primary-color)] transition-all duration-300"><a class="<?php if ($currentPage == '/') echo 'active_page'; ?>" href="/">Home</a></li>
+        <li class="hover:text-[var(--primary-color)] transition-all duration-300"><a class="about_p <?php if ($currentPage == '/about') echo 'active_page'; ?>" href="/about">About</a><i
                 class="fa-solid fa-chevron-down text-xs"></i>
         </li>
-        <li class="hover:text-[var(--primary-color)] transition-all duration-300"><a
+        <li class="hover:text-[var(--primary-color)] transition-all duration-300"><a class="service_p <?php if ($currentPage == '/services' || str_starts_with($currentPage, '/services/')) echo 'active_page'; ?>"
                 href="/services">Services</a><i class="fa-solid fa-chevron-down text-xs"></i>
         </li>
-        <li class="hover:text-[var(--primary-color)]"><a href="/portfolio">Portfolio</a></li>
-        <li class="hover:text-[var(--primary-color)]"><a href="/clients">Our Clients</a></li>
-        <li class="hover:text-[var(--primary-color)]"><a href="/blog">Blog</a></li>
-        <li class="hover:text-[var(--primary-color)]"><a href="/contact">Contact</a></li>
+        <li class="hover:text-[var(--primary-color)]"><a class="<?php if ($currentPage == '/portfolio') echo 'active_page'; ?>" href="/portfolio">Portfolio</a></li>
+        <li class="hover:text-[var(--primary-color)]"><a class="<?php if ($currentPage == '/clients') echo 'active_page'; ?>" href="/clients">Our Clients</a></li>
+        <li class="hover:text-[var(--primary-color)]"><a class="<?php if ($currentPage == '/blogs' || str_starts_with($currentPage, '/blogs/')) echo 'active_page'; ?>" href="/blogs">Blog</a></li>
+        <li class="hover:text-[var(--primary-color)]"><a class="<?php if ($currentPage == '/contact') echo 'active_page'; ?>" href="/contact">Contact</a></li>
     </ul>
 </div>
