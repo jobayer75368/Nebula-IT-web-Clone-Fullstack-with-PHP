a2de4cc35f3dcbca96c4e233cbeff657d854c2f5
Jobayer75368 <jobayer30325@gmail.com>
2026-06-24 13:21:47 +0600

    page loading progress bar added

--- frontend/includes/head.php
+++ frontend/includes/head.php
@@ -51,6 +51,14 @@ $currentPage = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
+
+
+    <!-- Include Pace JS -->
+    <script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
+    <!-- Include a Theme (e.g., Default Minimal) -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css">
+
+
     <style>
         .page_hero {
             background: url(<?= !empty($settings['hero_cover']) ? BASE_URL . 'uploads/' . $settings['hero_cover'] : ''; ?>);
